ae4c3b42bcd590400cd0f515ad592abce93bd1cf
hauteciel <hauteciel@gmail.com>
2025-05-08 10:37:39 +0900

    init

---
+++ src/main/java/com/lec/spring/controller/HomeController.java
@@ -0,0 +1,40 @@
+package com.lec.spring.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HomeController {
+
+    @Value("${env.info}")
+    private String envInfo;
+
+    @GetMapping("/aws/v2")
+    public String hello(@RequestParam(defaultValue = "1") Integer num){
+        if(num == 1) {
+            log.info("💚[" + envInfo + "]/aws/v2 호출. info로그 ");
+        } else if(num == -1){
+            log.error("💥[" + envInfo + "]/aws/v2 호출. error로그 ");
+        } else if(num == 0){
+            log.warn("💛[" + envInfo + "]/aws/v2 호출. warning로그 ");
+        }
+
+        return "<h1>AWS 배포 v2</h1>";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
